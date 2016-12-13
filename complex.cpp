c807bee751b1bf1784d3763f0a93f8851df787fc
Wojtek Niedzielski <nwojtek@gmail.com>
2016-12-13 19:56:10 +0100

    Operato +

--- complex.cpp
+++ complex.cpp
@@ -15,19 +15,23 @@ int operator*(int a, ComplexC C)
 {
   return C.getReal()*a;
 }
-  
+int operator+(int a, ComplexC C)
+{
+    return C.getReal()+a;
+}
+
 int main()
 {
     ComplexC a(1,1);
     ComplexC b(2,1);
-        
+
     cout <<"++++COMPLEX++++"<<endl;
     cout<<a<<endl;
     a.showParams();
     cout<<b<<endl;
     b.showParams();
     cout <<"++++++++++++++"<<endl;
-    
+
         cout <<a<<endl<<b<<endl;
     cout <<"-----DODAWANIE-----"<<endl;
     cout <<"c=a+b:\t";
@@ -36,7 +40,7 @@ int main()
     cout <<"a+=b+=c:\t";
     a+=b+=c;
     cout <<a<<endl<<endl;
-            
+
         cout <<a<<endl<<b<<endl<<c<<endl;
     cout <<"-----ODEJMOWANIE-----"<<endl;
     cout <<"c=a-b:\t";
@@ -44,7 +48,7 @@ int main()
     cout <<"a-=b:\t";
     a-=b;
     cout <<a<<endl<<endl;
-    
+
         cout <<a<<endl<<b<<endl<<c<<endl;
     cout <<"-----MNOZENIE-----"<<endl;
     cout <<"c=a*b:\t";
@@ -57,19 +61,19 @@ int main()
     a=10*a;
     cout <<a<<endl<<endl;
 
-    cout <<a<<endl<<b<<endl<<c<<endl;    
+    cout <<a<<endl<<b<<endl<<c<<endl;
     cout <<"-----POROWNANIE-----"<<endl;
     bool czy = a==b;
     cout <<"a==b:\t"<<czy<<endl;
     czy = b==c;
     cout <<"b==c:\t"<<czy<<endl<<endl;
-    
+
     cout <<a<<endl<<b<<endl<<c<<endl;
     cout <<"----DZIELENIE----"<<endl;
     cout <<"c=a/b:\t";
     cout <<c<<endl;
     cout <<"a/=b:\t";
     cout <<a<<endl;
-  
+
   return 0;
 }
