f5c09769046122b2feb39ab54d540417b40a3590
Wojtek Niedzielski <nwojtek@gmail.com>
2017-01-08 23:00:19 +0100

    Dodanie Insert i Clear! ;)

--- Tablica_Asocjacyjna/Asocjacja.cpp
+++ Tablica_Asocjacyjna/Asocjacja.cpp
@@ -18,3 +18,30 @@ Asocjacja::Asocjacja(const Asocjacja & prev)
     head->value=prev.head->value;
 
 }
+Asocjacja::~Asocjacja()
+{
+    head->key = NULL;
+}
+
+void Asocjacja::insert(const char *k, int value)
+{
+        char *key = new char[strlen (k) + 1];
+        char mark;
+
+        for(int i =0; i<k+1;i++)
+        {
+                mark = *(k+i);
+                *(key+i) = mark;
+        }
+        *(key+strlen(k)+1) = '\0';
+}
+
+void Asocjacja::clear()
+{
+    while(head)
+    {
+        list *t = head ->next;
+        delete head;
+        head = t;
+    }
+}
