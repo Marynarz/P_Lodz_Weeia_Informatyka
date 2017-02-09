c152e251410f04c80bed2bb34fa7c27227dcfac7
Wojtek Niedzielski <nwojtek@gmail.com>
2017-02-09 21:14:06 +0100

    Dodanie glownego programu

---
+++ Zaliczenie/Noga/Zaliczenie.cpp
@@ -0,0 +1,14 @@
+/* GitHUB: MARYNARZ REPO: P_LODZ_WEEIA_INFORMATYKA */
+#include <iostream>
+#include "Noga.hpp"
+
+using namespace std;
+
+int main()
+{
+    Noga n1;
+    n1.UstawDlugosc(4);
+    cout <<n1.OdczytajDlugosc()<<endl;
+    
+    return 0;
+}
