a7ad8142705ad3a24c1dcff4bf65673f7efb0e94
Wojtek Niedzielski <nwojtek@gmail.com>
2017-02-10 18:27:33 +0100

    Klasa stol juz dopieszczona ;), dodane sa tez komantarze

--- Zaliczenie/Noga/Stol.cpp
+++ Zaliczenie/Noga/Stol.cpp
@@ -3,37 +3,37 @@
     
 Stol::Stol(unsigned const int iloscNog):iloscNog(iloscNog)
 {
-    no = new Noga[iloscNog];
+    no = new Noga[iloscNog];    //tworzenie tablicy obiektow typu Noga o zadanym rozmiarze
 }
 Stol::~Stol()
 {
-    delete [] no;
+    delete [] no;   //kasowanie tablicy obiektow typu Noga
 }
-void Stol::CzyStoiRowno()
+void Stol::CzyStoiRowno() const
 {
-    bool czyRowne=true;
+    bool czyRowne=true;     //zmienna logiczna - zlaozenie ze wszystkie nogi sa rowne
     for(int i=0;i<iloscNog-1;i++)
     {
-        if(no[i].OdczytajDlugosc()!=no[i+1].OdczytajDlugosc())
-            czyRowne=false;
+        if(no[i].OdczytajDlugosc()!=no[i+1].OdczytajDlugosc())  //sprawdzamy czy dlogosc nog jest rowna
+            czyRowne=false;                                     // jesli nie to ustawiamy false
     }
     
-    if(czyRowne)
+    if(czyRowne)    //wypisywanie
         cout <<"Stoi Prosto!"<<endl;
     else
         cout <<"Stoi Krzywo!"<<endl;
 }
-void Stol::UstawDlugoscNogi(unsigned const int ktoraNoga, unsigned const int dlugoscNogi)
+void Stol::UstawDlugoscNogi(unsigned const int ktoraNoga, unsigned const int dlugoscNogi) const
 {
-    try
+    try     //zabezpiecznie jesli uzytkownik stwierdzi ze bedzie pisal poza pamiecia dozwolona
     {
         const char * wyjatek = "BladNog";
-        if(ktoraNoga>iloscNog || ktoraNoga<1)
-            throw wyjatek;
+        if(ktoraNoga>iloscNog || ktoraNoga<1)   //sprawdzanie czy mozliwa jest operacja
+            throw wyjatek;  //jelsi nie to wywalamy EXCEPTION, operacja nie zostanie wykonana
     
         no[ktoraNoga-1].UstawDlugosc(dlugoscNogi);
     }
-    catch(const char * wyj)
+    catch(const char * wyj)//lapiemy nasz EXCEPTION, opisujemy problem
     {
         cout <<"Exception: "<<wyj<<": Podana noga: "<<ktoraNoga<<"; Zakres dopuszczalny: 1-"<<iloscNog<<endl;
     }
