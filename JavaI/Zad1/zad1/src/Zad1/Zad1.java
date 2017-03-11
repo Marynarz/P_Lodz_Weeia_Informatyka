da6992840e5cef33e7d048fbefc9aa7f8fb4eaf0
Wojciech Niedzielski <Wojtek@PNMacBook-Air.local>
2017-03-11 09:21:58 +0100

    Pierwszy odpalenie, dziala

--- JavaI/Zad1/zad1/src/Zad1/Zad1.java
+++ JavaI/Zad1/zad1/src/Zad1/Zad1.java
@@ -5,19 +5,26 @@ public class Zad1 {
 
 	public static void main(String[] args) {
 		Silnik s1 = new Silnik();
-		Silnik.coSieDzieje wynik = s1.Zgaduj();
-		if(wynik == Silnik.coSieDzieje.ok)
+		Boolean gra = true;
+		while(gra)
 		{
-			System.out.println("Jest ok!");
+			Silnik.coSieDzieje wynik = s1.Zgaduj();
+			if(wynik == Silnik.coSieDzieje.ok)
+			{
+				System.out.println("Jest ok!");
+				gra = false;
+			}
+			else if (wynik == Silnik.coSieDzieje.zaDuzo)
+			{
+				System.out.println("Za duzo!");
+			}
+			else if (wynik == Silnik.coSieDzieje.zaMalo)
+			{
+				System.out.println("Za malo!");
+			}
 		}
-		else if (wynik == Silnik.coSieDzieje.zaDuzo)
-		{
-			System.out.println("Za duzo!");
-		}
-		else if (wynik == Silnik.coSieDzieje.zaMalo)
-		{
-			System.out.println("Za malo!");
-		}
+		s1.Statystyki();
+		
 		return;
 	}
 
