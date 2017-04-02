fdbc8be8c4a1a3f2c8196972c41f51537e4fd124
Wojciech Niedzielski <Wojtek@PNMacBook-Air.local>
2017-04-02 11:51:45 +0200

    Poczatek zadania 5

---
+++ JavaI/Zad1/zad5/src/zad5/test.java
@@ -0,0 +1,13 @@
+package zad5;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		vektory v1 = new vektory();
+		v1.dodawanieDanych();
+		
+		return;
+	}
+
+}
