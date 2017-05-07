09f6e6f82b8e687d015bf6f4f3cb141d90e0b57c
Wojciech Niedzielski <Wojtek@PNMacBook-Air.local>
2017-05-07 12:02:03 +0200

    Zad7 prawie

---
+++ JavaI/Zad1/zad7/src/cars/zad7.java
@@ -0,0 +1,11 @@
+package cars;
+
+public class zad7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Autoshop sklep = new Autoshop();
+		sklep.getCatalogue();
+	}
+
+}
