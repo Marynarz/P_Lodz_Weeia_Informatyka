1e4ae9067343a00e583cbd48b1588d17ddfb04df
Wojciech Niedzielski <Wojtek@PNMacBook-Air.local>
2017-05-11 20:47:27 +0200

    Dodanie exceptiona o zlym numerze, I have no idea, what I'm doing

---
+++ JavaI/Zad1/zad8/src/firstLocal/WrongNumberException.java
@@ -0,0 +1,62 @@
+package firstLocal;
+
+import java.util.Scanner;
+
+public class WrongNumberException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public WrongNumberException(int bNum, Building b){
+		System.out.println("WRONG_NUMBER_EXCEPTION: Wrong Building number: "+bNum);
+	}
+	public WrongNumberException(int bNum, Floor f){
+		System.out.println("WRONG_NUMBER_EXCEPTION: Wrong Floor number: "+bNum);
+	}
+	public WrongNumberException(int bNum, Room r){
+		System.out.println("WRONG_BUILDING_EXCEPTION: Wrong Room number: "+bNum);
+	}
+	public void setProperNumber(Building b){
+		System.out.println("Do you want set proper buildingNumber? (Y/N); ");
+		Scanner stdWejscie = new Scanner(System.in);
+		char answer = stdWejscie.nextLine().charAt(0);
+		if(answer == 'Y' || answer == 'y'){
+			System.out.println("Write number grater than 0: ");
+			int bNum = stdWejscie.nextInt();
+			b.setBuildingNumber(bNum);
+		}
+		else{
+			System.out.println("Foch z przytupem i melodyjka!");
+		}
+		stdWejscie.close();
+	}
+	public void setProperNumber(Floor f){
+		System.out.println("Do you want set proper buildingNumber? (Y/N); ");
+		Scanner stdWejscie = new Scanner(System.in);
+		char answer = stdWejscie.nextLine().charAt(0);
+		if(answer == 'Y' || answer == 'y'){
+			System.out.println("Write number grater than 0: ");
+			int bNum = stdWejscie.nextInt();
+			f.setFloorNumber(bNum);
+		}
+		else{
+			System.out.println("Foch z przytupem i melodyjka!");
+		}
+		stdWejscie.close();
+	}
+	public void setProperNumber(Room r){
+		System.out.println("Do you want set proper buildingNumber? (Y/N); ");
+		Scanner stdWejscie = new Scanner(System.in);
+		char answer = stdWejscie.nextLine().charAt(0);
+		if(answer == 'Y' || answer == 'y'){
+			System.out.println("Write number grater than 0: ");
+			int bNum = stdWejscie.nextInt();
+			r.setRoomNumber(bNum);
+		}
+		else{
+			System.out.println("Foch z przytupem i melodyjka!");
+		}
+		stdWejscie.close();
+	}
+}
