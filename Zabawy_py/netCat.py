11c7e531eeeb2b0daeebff2cb935b0d4b2771e2a
Wojtek Niedzielski <nwojtek@gmail.com>
2017-08-21 16:02:03 +0200

    Netcat podstawy

--- Zabawy_py/netCat.py
+++ Zabawy_py/netCat.py
@@ -26,3 +26,33 @@ def main():
   global target
   global upload_destination
   global port
+  
+  #czy sa podane argumenty?
+  if not len(sys.argv[1:]):
+    usage()
+    
+  #odczytujemy z wiersza polecen
+  try:
+    opts,args = getopt.getopt(sys.argv[1:],"hle:t:p:cu:",["help","listen","execute","target","port","command","upload"])
+  except getopt.GetoptError as err:
+    print str(err)
+    usage()
+  
+  #ustawianie parametrow
+  for o,a in opts:
+    if o in ("-h","--help"):
+      usage()
+    elif o in ("-l","--listen"):
+      listen = True
+    elif o in ("-e","--exceute"):
+      execute = a
+    elif o in ("-c","--command"):
+      command = True
+    elif o in ("-u","--upload"):
+      upload_destination = a
+    elif o in ("-t","--target"):
+      target = a
+    elif o in ("-p","--port"):
+      port = inta(a)
+    else:
+      assert False,"blad!"
