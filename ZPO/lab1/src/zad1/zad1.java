3e31401d6d4752bb0ea4b95a41ff16b42e1a6492
Wojtek <nwojtek@gmail.com>
2018-04-15 15:45:22 +0200

    ZPO lab 1, robione z1 i z2

---
+++ ZPO/lab1/src/zad1/zad1.java
@@ -0,0 +1,14 @@
+package zad1;
+
+public class zad1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		calc Calc = new calc("11011011");
+		System.out.println(Calc.getBin());
+		System.out.println(Calc.getOct());
+		System.out.println(Calc.getDec());
+		System.out.println(Calc.getHex());
+	}
+
+}
